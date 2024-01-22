d4493cca756bb7cad4b420c3b1be3b12c6b99199
Sreejith N <jithupreethy@gmail.com>
2024-01-22 21:35:41 +0530

    removed unwanted codes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -55,15 +55,6 @@ exports.getAllTours = async (req, res) => {
       message: err,
     });
   }
-  //   console.log(req.requestTime);
-  // res.status(200).json({
-  //   // Jsend format
-  //   status: "success",
-  //   results: tours.length,
-  //   data: {
-  //     tours,
-  //   },
-  // });
 };
 
 exports.getTour = async (req, res) => {
@@ -81,12 +72,6 @@ exports.getTour = async (req, res) => {
       message: err,
     });
   }
-  // res.status(200).json({
-  //   status: "success",
-  //   data: {
-  //     tour: "id",
-  //   },
-  // });
 };
 
 exports.createTours = async (req, res) => {
@@ -104,27 +89,6 @@ exports.createTours = async (req, res) => {
       message: err,
     });
   }
-  //   console.log(req.body);
-  // newId = tours[tours.length - 1].id + 1;
-  // newTour = Object.assign(
-  //   {
-  //     id: newId,
-  //   },
-  //   req.body
-  // );
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   `${__dirname}/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   (err) => {
-  //     res.status(201).json({
-  //       staus: "success",
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   }
-  // );
 };
 
 exports.updateTour = async (req, res) => {
@@ -145,12 +109,6 @@ exports.updateTour = async (req, res) => {
       message: err,
     });
   }
-  // res.status(200).json({
-  //   status: "success",
-  //   data: {
-  //     tour: "Updated tour",
-  //   },
-  // });
 };
 
 exports.deleteTour = async (req, res) => {
@@ -166,10 +124,4 @@ exports.deleteTour = async (req, res) => {
       message: err,
     });
   }
-  // res.status(200).json({
-  //   status: "success",
-  //   data: {
-  //     tours,
-  //   },
-  // });
 };
