e9ce7f535b49b10c1e5353cea520c634860ea075
Sreejith N <jithupreethy@gmail.com>
2024-01-23 16:48:31 +0530

    handeling invalid id error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -29,6 +36,8 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorPod(err, res);
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    sendErrorPod(error, res);
   }
 };
