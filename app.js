835e007801bea9ee8176a1d33ffd3e070bb7b80f
Sreejith N <jithupreethy@gmail.com>
2024-01-23 14:50:24 +0530

    adding global error functions

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require("express");
+
 // Logging module
 const morgan = require("morgan");
 
 const app = express();
+
 // Data from body is added to reqest (middelware)
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+// GLOBAL ERROR HANDELING
+app.use(globalErrorHandler);
 
 module.exports = app;
