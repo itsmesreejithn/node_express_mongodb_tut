35a576defd462624f089c572da3c9dfe0ba142ea
Sreejith N <jithupreethy@gmail.com>
2024-01-23 16:41:49 +0530

    seprate how errors should be shown for production and developement servers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,34 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorPod = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("Error", err);
+    res.status(500).json({
+      status: "error",
+      message: "Somting went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorPod(err, res);
+  }
 };
