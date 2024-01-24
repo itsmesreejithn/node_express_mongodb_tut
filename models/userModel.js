5c5504786d5b4051f37b02aca8ab863909a4e009
Sreejith N <jithupreethy@gmail.com>
2024-01-24 09:44:16 +0530

    created user model

--- models/userModel.js
+++ models/userModel.js
@@ -8,11 +8,22 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "User must have an email"],
+    unique: true,
+    lowercase: true,
     validate: validator.isEmail,
   },
   photo: String,
   password: {
     type: String,
-    validate: validator.isStrongPassword,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+  },
+  passwordComfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
   },
 });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
