7c856f08ba206108c41a04038b07cf3d9edc662b
Sreejith N <jithupreethy@gmail.com>
2024-01-30 11:18:00 +0530

    added reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,25 @@
+const catchAsync = require("../utils/catchAsync");
+const Review = require("../models/reviewModel");
+// const AppError = require("../utils/appError");
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  console.log("inide gellAllReviews");
+  const reviews = await Review.find();
+  console.log(reviews);
+  res.status(200).json({
+    status: "success",
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      review: newReview,
+    },
+  });
+});
