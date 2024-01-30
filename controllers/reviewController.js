5b7bdc53444285812535155aca2954640930c81e
Sreejith N <jithupreethy@gmail.com>
2024-01-30 12:14:56 +0530

    small error fixed while merging

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
-const catchAsync = require("../utils/catchAsync");
-const Review = require("../models/reviewModel");
-
-exports.createReview = catchAsync(async (req, res, next) => {
-  // ALLOW NESTED ROUTERS
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
-
-  const newReview = await Review.create(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      review: newReview,
-    },
-  });
-});
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourID };
-  const reviews = await Review.find();
-  res.status(200).json({
-    status: "success",
-    data: {
-      reviews,
-    },
-  });
-});
-=======
-const Review = require('./../models/reviewModel');
-const factory = require('./handlerFactory');
+const Review = require("./../models/reviewModel");
+const factory = require("./handlerFactory");
 // const catchAsync = require('./../utils/catchAsync');
 
 exports.setTourUserIds = (req, res, next) => {
@@ -43,4 +14,3 @@ exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
->>>>>>> revert_branch
