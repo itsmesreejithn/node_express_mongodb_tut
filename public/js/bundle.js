23ccd7502698786e43a72746fecdd70803d19c82
Sreejith N <jithupreethy@gmail.com>
2024-02-01 15:50:03 +0530

    rendered the user bookings

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12396,7 +12396,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32971" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46165" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
